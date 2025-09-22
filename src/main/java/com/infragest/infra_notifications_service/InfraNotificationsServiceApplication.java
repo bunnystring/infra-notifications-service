17b8fca25b276a88d210b79f91227d1410f71dd6
bunnystring <ziknaf_548@hotmail.com>
2025-09-21 21:36:07 -0500

    Add infra-notifications-service project with basic server configuration

---
+++ src/main/java/com/infragest/infra_notifications_service/InfraNotificationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.infragest.infra_notifications_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraNotificationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraNotificationsServiceApplication.class, args);
+	}
+
+}
